aec75afc76202ee7deea3cab8dbf4febbdbfa7bc
Paweł Ksieniewicz <pawel.ksieniewicz@pwr.edu.pl>
2015-04-22 13:59:26 +0200

    Eureka

--- work.m
+++ work.m
@@ -2,11 +2,11 @@ warning off
 clear
 clf
 
-for image_id = 1;
+for image_id = [1];
 wideness = 1;
 similarity_treshold = 0.025;
-amount = 250;
-blur_size = 5;
+amount = 1000;
+blur_size = 10;
 
 [tmp tmp2 tmp3] = natix(image_id, wideness, similarity_treshold, amount, blur_size);
 
