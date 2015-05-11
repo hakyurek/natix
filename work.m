0698e027b386fd08eb4e6fc8b74ebee589101dab
Paweł Ksieniewicz <pawel.ksieniewicz@pwr.edu.pl>
2015-05-11 07:37:24 +0200

    NX

--- work.m
+++ work.m
@@ -2,11 +2,11 @@ warning off
 clear
 clf
 
-for image_id = [ 3 ];
-wideness = 1;
-similarity_treshold = 0.025;
-amount = 10000;
-blur_size = 10;
+for image_id = [ 1 ];
+wideness = 2;                   %1-2 2-1
+similarity_treshold = 0.04;     %1-0.04 2-0.08
+amount = 1000;                  %1-1000 2-1000
+blur_size = 2;                  %1-2 2-2
 
 [tmp tmp2 tmp3] = natix(image_id, wideness, similarity_treshold, amount, blur_size);
 
